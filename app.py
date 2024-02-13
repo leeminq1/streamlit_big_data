340210912ca0ecce6084576bf70fd6e80ed241c0
leeminq1 <min2195@naver.com>
2024-02-13 19:17:48 +0900

    12453

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from st_pages import Page, show_pages, add_page_title,add_indentation
 
 show_pages(
     [
-        Page("app.py", "Comment", "🔥"),
-        Page("pages/age_salary.py", "연령별 임금 및 근로시간", "📣"),
+        Page("/mount/src/streamlit_big_data/app.py", "Comment", "🔥"),
+        Page("/mount/src/streamlit_big_data/pages/age_salary.py", "연령별 임금 및 근로시간", "📣"),
     ]
 )
 
